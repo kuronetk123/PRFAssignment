5c12e1356e476cc7463bdf702bb1a0f0c241fea0
codie <ngnhathuy1224@gmail.com>
2023-02-23 19:03:19 +0700

    Initial commit

---
+++ bai1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main() {
+	FILE* file = fopen ("./inputs/inputinput1.txt", "r");
+	int len1, len2, i;
+	
+	fscanf(file, "%d", &len1);
+	int arr1[len1];
+	for(i= 0; i != len1; i++) fscanf(file, "%d", (arr1+i));
+	
+	fscanf(file, "%d", &len2);
+	int arr2[len2];
+	for(i= 0; i != len2; i++) fscanf(file, "%d", (arr2+i));
+	
+	fclose(file);
+	
+	int resLength = len1 + len2;
+	int res[resLength];
+	
+	len1--; len2--;
+	i= 0;
+	while(len1 != -1 && len2 != -1) {
+		if(arr1[len1] >= arr2[len2]){
+			res[i] = arr1[len1];
+			len1--;
+		} else {
+			res[i] = arr2[len2];
+			len2--;
+		}
+		i++;
+	}
+	
+	while(len1 != -1) {
+		res[i] = arr1[len1];
+		len1--; i++;
+	}
+	
+	while(len2 != -1) {
+		res[i] = arr2[len2];
+		len2--; i++;
+	}
+	
+	printf("Two join arrays in descending order: ");
+	for(i= 0; i != resLength; i++) printf("%d ", res[i]);
+	printf("\n");
+	
+	return 0;
+}
